054451aeb1942de2540f54686bd164fcb7145397
Anmol Sharma <demondevil630@gmail.com>
2021-11-30 17:24:34 -0500

    Repo Structure change and Hosted on PlayStore!!!

--- app/src/main/java/com/example/realestateapp/Property.kt
+++
@@ -1,5 +0,0 @@
-package com.example.realestateapp
-
-class Property(var id: String?=null, var address : String?=null, var postalCode : String?=null, var city : String?=null
-               , var bed : Int?=null, var bath : Int?=null, var room : Int?=null
-)
